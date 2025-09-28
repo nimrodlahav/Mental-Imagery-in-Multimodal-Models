13ab885a0b13dea04f449429930395d233f046a6
nimrodlahav <128465134+nimrodlahav@users.noreply.github.com>
2025-09-28 15:10:29 +0300

    Update __init__.py

--- src/vlm_activations/__init__.py
+++ src/vlm_activations/__init__.py
@@ -1,1 +1,5 @@
-
+# src/vlm_activations/__init__.py
+from .model import load_model
+from .data import load_batch  # or your extract_twenty() if you kept that name
+from .preprocess import text_inputs, image_inputs
+from .collect import collect_activations_one_pass, parquet_writer
